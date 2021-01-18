3a9dd66d82cb11aadb480ac4435327fab2a16ad2
Jeffrey Buehler <jeffreydeanbuehler@gmail.com>
2021-01-18 09:59:13 -0700

    include attack of the clones script

--- src/app/structures/enums.ts
+++ src/app/structures/enums.ts
@@ -20,7 +20,7 @@ export enum AnswerState {
 
 export enum MovieTextCode {
   phantom = '1 phantom menace script.txt',
-  attack = 'attack',
+  attack = '2 Attack of the Clones.txt',
   revenge = '3 Revenge of the Sith.txt',
   hope = '4 A New Hope.txt',
   empire = 'empire',
